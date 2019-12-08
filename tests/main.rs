5c9edcb5ce03a5d9903c905f069e8c23bf325372
Jihyun Yu <yjh0502@gmail.com>
2019-12-08 18:25:50 +0900

    add testcase for encode_async

--- tests/main.rs
+++ tests/main.rs
@@ -4,6 +4,12 @@ mod tests {
     use std::io::Read;
     use xdelta3::*;
 
+    fn encode2(input: &[u8], src: &[u8]) -> Option<Vec<u8>> {
+        let mut out = Vec::new();
+        futures::executor::block_on(encode_async(input, src, &mut out)).expect("failed to decode");
+        Some(out)
+    }
+
     fn decode2(input: &[u8], src: &[u8]) -> Option<Vec<u8>> {
         let mut out = Vec::new();
         futures::executor::block_on(decode_async(input, src, &mut out)).expect("failed to decode");
@@ -26,29 +32,36 @@ mod tests {
         assert_eq!(&recode, &[1, 2, 3, 4, 5, 6, 7]);
     }
 
+    fn read_file(filename: &str) -> Vec<u8> {
+        let mut file = File::open(filename).expect("Failed to open file");
+        let mut data = Vec::new();
+
+        file.read_to_end(&mut data).expect("Failed to read file");
+
+        data
+    }
+
     #[test]
     fn xdelta_own_test() {
         let fixure_path = "xdelta3/xdelta3/examples/iOS/xdelta3-ios-test/xdelta3-ios-test/";
-        let original_fixure = format!("{}/{}", fixure_path, "file_v1.bin");
-        let correct_fixure = format!("{}/{}", fixure_path, "file_v2.bin");
-        let patch_fixure = format!("{}/{}", fixure_path, "file_v1_to_v2.bin");
-        let mut original_fixure = File::open(original_fixure).expect("Failed to open file");
-        let mut original_data = Vec::new();
-        let mut patch_fixure = File::open(patch_fixure).expect("Failed to open patch");
-        let mut patch_data = Vec::new();
-        let mut correct_fixure = File::open(correct_fixure).expect("Failed to open reference");
-        let mut correct_data = Vec::new();
-        original_fixure
-            .read_to_end(&mut original_data)
-            .expect("Failed to read file");
-        patch_fixure
-            .read_to_end(&mut patch_data)
-            .expect("Failed to read file");
-        correct_fixure
-            .read_to_end(&mut correct_data)
-            .expect("Failed to read file");
+        let original_data = read_file(&format!("{}/{}", fixure_path, "file_v1.bin"));
+        let correct_data = read_file(&format!("{}/{}", fixure_path, "file_v2.bin"));
+        let patch_data = read_file(&format!("{}/{}", fixure_path, "file_v1_to_v2.bin"));
 
         let patched_data = check_decode(&patch_data, &original_data);
         assert_eq!(patched_data, correct_data);
+    }
+
+    #[test]
+    fn round_trip_test() {
+        let fixure_path = "xdelta3/xdelta3/examples/iOS/xdelta3-ios-test/xdelta3-ios-test/";
+        let source = read_file(&format!("{}/{}", fixure_path, "file_v1.bin"));
+        let input = read_file(&format!("{}/{}", fixure_path, "file_v2.bin"));
+
+        let patch_sync = encode(&input, &source).expect("failed to encode");
+        assert_eq!(input, check_decode(&patch_sync, &source));
+
+        let patch_async = encode2(&input, &source).expect("failed to encode");
+        assert_eq!(input, check_decode(&patch_async, &source));
     }
 }
